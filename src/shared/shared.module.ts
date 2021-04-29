abfc3b2d5a196b6e5b249dd478ee9b16cd65189f
Sanyi Tóth <sandor.toth@hotmail.com>
2021-04-29 19:29:35 +0200

    3.HiddenDirective with isHidden field

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HighlightDirective} from './directives/highlight.directive';
+import { HiddendirectiveDirective } from './directives/hiddendirective.directive';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HighlightDirective,
+    HiddendirectiveDirective
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HighlightDirective,
+    HiddendirectiveDirective
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
